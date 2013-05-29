bd258b0237ea99612b424a1fb05c60e4984f4213
Giulio Eulisse <giulio.eulisse@gmail.com>
2013-05-29 15:33:36 +0200

    Do not install cmssw-ib if version does not start with CMSSW.

--- autoInstall.sh
+++ autoInstall.sh
@@ -51,7 +51,7 @@ for WEEK in 0 1; do
   (
     source $WORKDIR/$SCRAM_ARCH/external/apt/*/etc/profile.d/init.sh ;
     apt-get update ;
-    apt-cache search cmssw-ib | cut -d\  -f1 | sort > onserver$$.txt ;
+    apt-cache search cmssw-ib\\+CMSSW | cut -d\  -f1 | sort > onserver$$.txt ;
     rpm -qa --queryformat '%{NAME}\n' | grep cmssw-ib | sort > installed$$.txt ;
     for x in `diff -u onserver$$.txt installed$$.txt | grep -e '^-' | sed -e 's/^-//'`; do
       apt-get install -q -y $x ;
