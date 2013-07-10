d357fc38fb7e342806af50d7ea73c1cf0a17eac5
Shahzad Malik Muzaffar <Shahzad.Malik.Muzaffar@cern.ch>
2013-07-10 15:26:06 +0200

    autoInstallLogs.sh: First change to log directory before unpacking the logAnalysis.pkl

--- autoInstallLogs.sh
+++ autoInstallLogs.sh
@@ -21,7 +21,8 @@ for WEEK in 0 1; do
     fi
     mkdir -p $REL_LOGS || echo "Cannot create directory for $REL_LOGS"
     rsync -a --no-group --no-owner cmsbuild@cmsrep.cern.ch:/data/cmssw/cms.week$WEEK/WEB/build-logs/$SCRAM_ARCH/$CMSSW_NAME/logs/html/ $REL_LOGS/ || echo "Unable to sync logs in $REL_LOGS."
-    # Decompress logs if they are compressed.
-    tar xzf html-logs.tgz ./logAnalysis.pkl || echo "Unable to unpack logs in $REL_LOGS."
+    pushd $REL_LOGS
+      tar xzf html-logs.tgz ./logAnalysis.pkl || echo "Unable to unpack logs in $REL_LOGS."
+    popd
   done
 done
